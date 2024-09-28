77602dbf0cb637c22fc4416996f68cc548ff0468
aqilakmal <faqih.xv@gmail.com>
2024-09-28 12:28:31 +0800

    Added types for standardisation

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,44 @@ export interface User {
   }
   createdAt?: Date;
 }
+
+export enum ListingCategory {
+  ELECTRONICS = 'Electronics',
+  CLOTHING_AND_ACCESSORIES = 'Clothing & Accessories',
+  BAGS_AND_WALLETS = 'Bags & Wallets',
+  PERSONAL_IDENTIFICATION_AND_DOCUMENTS = 'Personal Identification & Documents',
+  PETS = 'Pets',
+  SPORTS_AND_FITNESS = 'Sports & Fitness',
+  TOYS_AND_CHILDREN_ITEMS = 'Toys & Children Items',
+  EYEWEAR = 'Eyewear',
+  VEHICLES = 'Vehicles',
+  MISCELLANEOUS = 'Miscellaneous',
+  OTHER = 'Other',
+}
+
+export enum ListingStatus {
+  ACTIVE = 'active',
+  RESOLVED = 'resolved',
+  ARCHIVED = 'archived',
+}
+
+export interface ListingLocation {
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
+export interface Listing {
+  id: string;
+  type: 'lost' | 'found';
+  userId: string;
+  title: string;
+  description: string;
+  images: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  expiresAt: Date;
+  locations: ListingLocation[];
+  status: ListingStatus;
+  category: ListingCategory;
+}
