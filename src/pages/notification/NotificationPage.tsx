b01209e319cf30daf34fcaeffa55ed776fdfb5b4
aqilakmal <faqih.xv@gmail.com>
2024-09-28 12:30:35 +0800

    Added dummy page

---
+++ src/pages/notification/NotificationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotificationPage: React.FC = () => {
+  return (
+    <div className="min-h-full bg-white p-4">
+      <h1 className="text-2xl font-bold mb-4">Notifications</h1>
+      <p>This is a dummy notification page. Implement your notification functionality here.</p>
+    </div>
+  );
+};
+
+export default NotificationPage;
