2b096e64aee5930154f612cbf7a32d71aedc2027
aqilakmal <faqih.xv@gmail.com>
2024-09-30 12:09:29 +0800

    Major refactor and use the new typing system

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useAtom } from 'jotai';
-import { listingsAtom, fetchListingsAtom, listingsLoadingAtom, listingsFetchedAtom } from '../stores/listingStore';
+import { listingsAtom, fetchListingsAtom, listingsFetchedAtom } from '../stores/listingStore';
 import { Listing, ListingStatus, SearchParams } from '../types';
 import { MagnifyingGlassIcon, ArrowPathIcon } from '@heroicons/react/24/outline';
+import { showCustomToast } from '../components/CustomToast';
 import ListingCard from '../components/ListingCard';
 import SearchDrawer from '../components/search/SearchDrawer';
 import ActiveFilters from '../components/search/ActiveFilters';
@@ -15,23 +16,37 @@ const defaultSearchParams: SearchParams = {
   category: '',
   status: '' as ListingStatus | '',
   sortBy: 'createdAt',
-  sortOrder: 'ascending',
+  sortOrder: 'descending',
   location: null,
 };
 
 export default function ListingPage() {
-  const [listings] = useAtom(listingsAtom);
+  const [listings, setListings] = useAtom(listingsAtom);
+  const [listingsFetched, setListingsFetched] = useAtom(listingsFetchedAtom);
   const [, fetchListings] = useAtom(fetchListingsAtom);
-  const [isListingsLoading] = useAtom(listingsLoadingAtom);
-  const [listingsFetched] = useAtom(listingsFetchedAtom);
   const [activeTab, setActiveTab] = useState<'all' | 'lost' | 'found'>('all');
   const [filteredListings, setFilteredListings] = useState<Listing[]>([]);
   const [isSearchDrawerOpen, setIsSearchDrawerOpen] = useState(false);
   const [searchParams, setSearchParams] = useState<SearchParams>(defaultSearchParams);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleRefresh = useCallback(async () => {
-    await fetchListings();
-  }, [fetchListings]);
+    console.log('[ListingPage] Refreshing listings...');
+    setIsLoading(true);
+    setListingsFetched(false);
+    try {
+      await fetchListings();
+    } catch (error) {
+      console.error('[ListingPage] Error refreshing listings:', error);
+      showCustomToast({
+        title: "Error Refreshing Listings",
+        description: "Please try again later.",
+        color: "danger",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }, [setListingsFetched, fetchListings]);
 
   const PullDownContent = () => (
     <div className="flex items-center justify-center space-x-2 text-blue-600 mt-8">
@@ -47,13 +62,37 @@ export default function ListingPage() {
     </div>
   );
 
+  // On mount, fetch listings
   useEffect(() => {
-    if (!listingsFetched) {
-      fetchListings();
-    }
-  }, [listingsFetched, fetchListings]);
 
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        await fetchListings();
+      } catch (error) {
+        console.error('[ListingPage] Error fetching listings:', error);
+        showCustomToast({
+          title: "Error Fetching Listings",
+          description: "Please try again later.",
+          color: "danger",
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    // If listings are not fetched, fetch them
+    if (!listingsFetched) {
+      fetchData();
+    } else {
+      setIsLoading(false);
+    }
+  }, [listings, listingsFetched, setListings, setListingsFetched, fetchListings]);
+
+  // On mount, fetch listings
   useEffect(() => {
+
+    // Filter listings based on active tab and search params
     const filtered = Object.values(listings).filter(listing => 
       (activeTab === 'all' || listing.type === activeTab) &&
       (searchParams.keyword === '' || listing.title.toLowerCase().includes(searchParams.keyword.toLowerCase()) ||
@@ -66,11 +105,14 @@ export default function ListingPage() {
     filtered.sort((a, b) => {
       const aValue = a[searchParams.sortBy as keyof Listing];
       const bValue = b[searchParams.sortBy as keyof Listing];
-      if (aValue < bValue) return searchParams.sortOrder === 'ascending' ? -1 : 1;
-      if (aValue > bValue) return searchParams.sortOrder === 'ascending' ? 1 : -1;
+      if (aValue && bValue) {
+        if (aValue < bValue) return searchParams.sortOrder === 'ascending' ? -1 : 1;
+        if (aValue > bValue) return searchParams.sortOrder === 'ascending' ? 1 : -1;
+      }
       return 0;
     });
 
+    // Set the filtered listings
     setFilteredListings(filtered);
   }, [listings, activeTab, searchParams]);
 
@@ -147,7 +189,7 @@ export default function ListingPage() {
                 className="h-full w-fit p-2 px-3.5 rounded-lg bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
                 onClick={() => setIsSearchDrawerOpen(true)}
               >
-                <MagnifyingGlassIcon className="h-5 w-5 text-white stroke-2" />
+                <MagnifyingGlassIcon className="h-5 w-5 text-white stroke-[3]" />
               </button>
             </div>
           </div>
@@ -157,7 +199,7 @@ export default function ListingPage() {
             onRemoveFilter={handleRemoveFilter}
           />
 
-          {isListingsLoading ? (
+          {isLoading ? (
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
               <LoadingSpinner />
             </div>
--- src/stores/listingStore.ts
+++ src/stores/listingStore.ts
@@ -1,13 +1,32 @@
 import { atom } from 'jotai';
-import { Listing, ListingImages } from '../types';
+import { atomWithStorage } from 'jotai/utils';
+import { Listing, ListingDB, ListingImages, Marker } from '../types';
 import { db } from '../firebaseConfig';
-import { collection, addDoc, updateDoc, deleteDoc, doc, getDocs } from 'firebase/firestore';
-import { addImage, deleteImage, getImage } from '../utils/imageUtils';
+import { collection, addDoc, updateDoc, deleteDoc, doc, getDocs, Timestamp } from 'firebase/firestore';
+import { addImage, deleteImage, getImage } from './imageStore';
+import { markersAtom, addMarker, fetchMarker } from './markerStore';
 
-// Updated listingsAtom
-export const listingsAtom = atom<Record<string, Listing>>({});
-export const listingsLoadingAtom = atom<boolean>(false);
-export const listingsFetchedAtom = atom<boolean>(false);
+export const listingsAtom = atomWithStorage<Record<string, Listing>>('listings', {});
+export const listingsFetchedAtom = atomWithStorage<boolean>('listingsFetched', false);
+
+/**
+ * @description Convert a ListingDB to a Listing
+ * @param {ListingDB} listingDB - The ListingDB object
+ * @param {Marker[]} markers - The markers for the listing
+ * @returns {Listing} - The converted Listing object
+ */
+const convertListingDBToListing = (listingDB: ListingDB, markers: Marker[]): Listing => ({
+  ...listingDB,
+  createdAt: listingDB.createdAt.toDate(),
+  updatedAt: listingDB.updatedAt.toDate(),
+  expiresAt: listingDB.expiresAt.toDate(),
+  images: {
+    main: { id: listingDB.images.mainId, listingId: listingDB.id, data: '' },
+    alt1: listingDB.images.alt1Id ? { id: listingDB.images.alt1Id, listingId: listingDB.id, data: '' } : undefined,
+    alt2: listingDB.images.alt2Id ? { id: listingDB.images.alt2Id, listingId: listingDB.id, data: '' } : undefined,
+  },
+  markers,
+});
 
 /**
  * @description Fetch all listings from Firestore
@@ -15,84 +34,159 @@ export const listingsFetchedAtom = atom<boolean>(false);
  */
 export const fetchListingsAtom = atom(
   null,
-  async (_, set) => {
-    console.log('[listingStore/fetchListingsAtom]: called');
-    set(listingsLoadingAtom, true);
+  async (get, set): Promise<Record<string, Listing>> => {
+    console.log("[listingStore/fetchListingsAtom] called");
     try {
       const querySnapshot = await getDocs(collection(db, 'Listings'));
+      console.log('[listingStore/fetchListingsAtom] 🔥');
       const listings: Record<string, Listing> = {};
+      const existingMarkers = get(markersAtom);
+
       for (const doc of querySnapshot.docs) {
-        const data = doc.data();
-        const listing: Listing = {
-          id: doc.id,
-          ...data,
-          createdAt: data.createdAt.toDate(),
-          updatedAt: data.updatedAt.toDate(),
-          expiresAt: data.expiresAt.toDate()
-        } as Listing;
+        const listingDB = doc.data() as ListingDB;
+        const markerIds = listingDB.markerIds;
+
+        // Fetch markers using the fetchMarker function
+        const markers = await Promise.all(
+          markerIds.map(async (markerId) => {
+            const marker = await fetchMarker(markerId, existingMarkers);
+            if (marker) {
+              // Update markersAtom with the new marker
+              set(markersAtom, (prev) => ({ ...prev, [markerId]: marker }));
+            }
+            return marker;
+          })
+        );
+
+        const listing = convertListingDBToListing({ ...listingDB, id: doc.id }, markers.filter(m => m !== null) as Marker[]);
 
         // Fetch the main image if it's not already loaded
-        if (listing.images.main.id && !listing.images.main.src) {
+        if (listing.images.main.id && !listing.images.main.data) {
           const imageDoc = await getImage(listing.images.main.id);
           if (imageDoc) {
-            listing.images.main.src = imageDoc.src;
+            listing.images.main.data = imageDoc.data;
           }
         }
 
         listings[doc.id] = listing;
       }
+      
       set(listingsAtom, listings);
       set(listingsFetchedAtom, true);
+      
+      return listings;
     } catch (error) {
-      console.error('[listingStore/fetchListingsAtom]: error:', error);
-    } finally {
-      set(listingsLoadingAtom, false);
+      console.error('[listingStore] Error fetching listings:', error);
+      throw error;
     }
   }
 );
 
 /**
  * @description Add a new listing to Firestore
- * @param {Omit<Listing, 'id' | 'images'> & { images: File[] }} newListing - The new listing to add
- * @returns {Promise<Listing>} - A promise that resolves when the listing is added
+ * @param {Omit<Listing, 'id' | 'images' | 'markers'>} newListing - The new listing to add
+ * @param {File[]} imageFiles - The image files to upload
+ * @param {Omit<Marker, 'id' | 'listingId'>[]} markers - The markers to add
+ * @returns {Promise<Record<string, Listing>>} - A promise that resolves when the listing is added
  */
 export const addListingAtom = atom(
   null,
-  async (_, set, newListing: Omit<Listing, 'id' | 'images'> & { images: File[] }) => {
-    console.log('[listingStore/addListingAtom]: newListing:', JSON.stringify(newListing));
+  async (get, set, params: {
+    newListing: Omit<Listing, 'id' | 'images' | 'markers'>,
+    imageFiles: File[],
+    markers: Omit<Marker, 'id' | 'listingId'>[]
+  }): Promise<Record<string, Listing>> => {
+    const { newListing, imageFiles, markers } = params;
+    console.log('[listingStore/addListing]: newListing:', JSON.stringify(newListing));
     try {
-      const imageIds: ListingImages = { main: { id: '' } };
+      const imageIds: { mainId: string; alt1Id?: string; alt2Id?: string } = { mainId: '' };
+
+      // Create the listing document first to get the listingId
+      const listingDB: Omit<ListingDB, 'id' | 'images' | 'markerIds'> = {
+        ...newListing,
+        createdAt: Timestamp.fromDate(newListing.createdAt),
+        updatedAt: Timestamp.fromDate(newListing.updatedAt),
+        expiresAt: Timestamp.fromDate(newListing.expiresAt),
+      };
+      const docRef = await addDoc(collection(db, 'Listings'), listingDB);
+      console.log('[listingStore/addListing] 🔥');
+      const listingId = docRef.id;
 
       // Upload images and get their IDs
-      for (let i = 0; i < newListing.images.length; i++) {
-        const file = newListing.images[i];
+      for (let i = 0; i < imageFiles.length; i++) {
+        const file = imageFiles[i];
         const base64Image = await new Promise<string>((resolve) => {
           const reader = new FileReader();
           reader.onloadend = () => resolve(reader.result as string);
           reader.readAsDataURL(file);
         });
 
-        const imageId = await addImage(base64Image);
+        const imageId = await addImage(base64Image, listingId);
         if (i === 0) {
-          imageIds.main.id = imageId;
+          imageIds.mainId = imageId;
         } else if (i === 1) {
-          imageIds.alt1 = { id: imageId };
+          imageIds.alt1Id = imageId;
         } else if (i === 2) {
-          imageIds.alt2 = { id: imageId };
+          imageIds.alt2Id = imageId;
         }
       }
 
-      const listingData = {
-        ...newListing,
+      // Add markers and get their IDs
+      const markerIds = await Promise.all(
+        markers.map(async (marker) => {
+          const newMarker = { ...marker, listingId };
+          const addedMarker = await addMarker(set, newMarker);
+          return addedMarker.id;
+        })
+      );
+
+      // Update the listing with imageIds and markerIds
+      await updateDoc(doc(db, 'Listings', listingId), {
         images: imageIds,
+        markerIds,
+      });
+      console.log('[listingStore/addListing] 🔥');
+
+      // Create the full Listing object
+      const listing: Listing = {
+        ...newListing,
+        id: listingId,
+        images: {
+          main: {
+            id: imageIds.mainId,
+            listingId,
+            data: await getImage(imageIds.mainId).then((img) => img?.data || ''),
+          },
+          alt1: imageIds.alt1Id
+            ? {
+                id: imageIds.alt1Id,
+                listingId,
+                data: await getImage(imageIds.alt1Id).then((img) => img?.data || ''),
+              }
+            : undefined,
+          alt2: imageIds.alt2Id
+            ? {
+                id: imageIds.alt2Id,
+                listingId,
+                data: await getImage(imageIds.alt2Id).then((img) => img?.data || ''),
+              }
+            : undefined,
+        },
+        markers: markers.map((m, index) => ({
+          ...m,
+          id: markerIds[index],
+          listingId,
+        })),
       };
 
-      const docRef = await addDoc(collection(db, 'Listings'), listingData);
-      const listing = { id: docRef.id, ...listingData } as Listing;
-      set(listingsAtom, prev => ({ ...prev, [docRef.id]: listing }));
-      return listing;
+      // Update the client-side state
+      console.log('[listingStore/addListing]: listingsAtom:', get(listingsAtom));
+      const updatedListings = { ...get(listingsAtom), [listingId]: listing };
+      set(listingsAtom, updatedListings);
+      console.log('[listingStore/addListing]: updatedListings:', updatedListings);
+      return updatedListings;
     } catch (error) {
-      console.error('[listingStore/addListingAtom]: error:', error);
+      console.error('[listingStore/addListing]: error:', error);
       throw error;
     }
   }
@@ -105,11 +199,12 @@ export const addListingAtom = atom(
  */
 export const updateListingAtom = atom(
   null,
-  async (_, set, updatedListing: Listing) => {
+  async (_, set, updatedListing: Listing): Promise<void> => {
     console.log('[listingStore/updateListingAtom]: updatedListing:', JSON.stringify(updatedListing));
     try {
       const { id, ...updateData } = updatedListing;
       await updateDoc(doc(db, 'Listings', id), updateData);
+      console.log('[listingStore/updateListingAtom] 🔥');
       set(listingsAtom, prev => ({ ...prev, [id]: updatedListing }));
     } catch (error) {
       console.error('[listingStore/updateListingAtom]: error:', error);
@@ -124,7 +219,7 @@ export const updateListingAtom = atom(
  */
 export const deleteListingAtom = atom(
   null,
-  async (get, set, listingId: string) => {
+  async (get, set, listingId: string): Promise<void> => {
     console.log('[listingStore/deleteListingAtom]: listingId:', listingId);
     try {
       const listing = get(listingsAtom)[listingId];
@@ -135,6 +230,7 @@ export const deleteListingAtom = atom(
       if (listing.images.alt2?.id) await deleteImage(listing.images.alt2.id);
 
       await deleteDoc(doc(db, 'Listings', listingId));
+      console.log('[listingStore/deleteListingAtom] 🔥');
       set(listingsAtom, prev => {
         const newListings = { ...prev };
         delete newListings[listingId];
@@ -155,7 +251,7 @@ export const deleteListingAtom = atom(
  */
 export const updateListingImageAtom = atom(
   null,
-  async (get, set, payload: { listingId: string; imageKey: keyof ListingImages; imageSrc: string }) => {
+  async (get, set, payload: { listingId: string; imageKey: keyof ListingImages; imageSrc: string }): Promise<void> => {
     const { listingId, imageKey, imageSrc } = payload;
     const listings = get(listingsAtom);
     const listing = listings[listingId];
@@ -172,10 +268,24 @@ export const updateListingImageAtom = atom(
         }
       };
 
-      set(listingsAtom, {
-        ...listings,
-        [listingId]: updatedListing
-      });
+      try {
+        // Update the listing in Firestore
+        await updateDoc(doc(db, 'Listings', listingId), {
+          [`images.${imageKey}.src`]: imageSrc
+        });
+        console.log('[listingStore/updateListingImageAtom] 🔥');
+
+        // Update the client-side state
+        set(listingsAtom, {
+          ...listings,
+          [listingId]: updatedListing
+        });
+      } catch (error) {
+        console.error('[listingStore/updateListingImage]: error:', error);
+        throw error;
+      }
+    } else {
+      throw new Error(`Listing with id ${listingId} not found`);
     }
   }
 );
