80cef3f1d61cb5182ffc6a4fb9585f487fc86c77
aqilakmal <faqih.xv@gmail.com>
2024-10-06 12:02:20 +0800

    Comment out react strict mode to prevent double renders

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+// import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from './App.tsx';
@@ -40,9 +40,9 @@ const theme = extendTheme({
 });
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </StrictMode>
+  // <StrictMode>
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>
+  // </StrictMode>
 );
