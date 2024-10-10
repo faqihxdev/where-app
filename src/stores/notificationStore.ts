1ad77eac426bc5944aa0ca4ce3cd330c9a0bf55d
aqilakmal <faqih.xv@gmail.com>
2024-10-10 22:11:33 +0800

    feat: use hash for notif id

--- src/stores/notificationStore.ts
+++ src/stores/notificationStore.ts
@@ -1,13 +1,13 @@
+import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
-import { SetStateAction } from 'jotai';
 import { db } from '../firebaseConfig';
 import {
   collection,
-  addDoc,
   doc,
   getDoc,
   getDocs,
   query,
+  setDoc,
   where,
   writeBatch,
 } from 'firebase/firestore';
@@ -20,93 +20,123 @@ export const userNotificationsAtom = atomWithStorage<Record<string, Notification
   {}
 );
 
+// Store notificationsLoaded in AtomStorage
+export const notificationsLoadedAtom = atomWithStorage<boolean>('notificationsLoaded', false);
+
 /**
  * @description Add a new notification to Firestore and update the userNotifications atom
  * @param {Omit<Notification, 'id'>} newNotification - The new notification to add
- * @param {Function} setNotifications - Function to update the userNotificationsAtom
  * @returns {Promise<Notification>} - A promise that resolves when the notification is added
  */
-export const addNotification = async (
-  newNotification: Omit<Notification, 'id' | 'status' | 'createdAt' | 'updatedAt' | 'expiresAt'>,
-  existingNotifications: Record<string, Notification>,
-  setNotifications: (update: SetStateAction<Record<string, Notification>>) => void
-): Promise<Notification> => {
-  console.log(
-    `[notificationStore/addNotification]: Adding notification: ${JSON.stringify(newNotification)}`
-  );
-  try {
-    // Don't add if notification with userId, title, message, and type already exists
-    const existingNotification = Object.values(existingNotifications).find(
-      (notification) =>
-        notification.userId === newNotification.userId &&
-        notification.title === newNotification.title &&
-        notification.message === newNotification.message &&
-        notification.type === newNotification.type
+export const addNotificationAtom = atom(
+  null,
+  async (
+    get,
+    set,
+    newNotification: Omit<Notification, 'id' | 'status' | 'createdAt' | 'updatedAt' | 'expiresAt'>
+  ) => {
+    console.log(
+      `[notificationStore/addNotificationAtom]: Adding: ${JSON.stringify(newNotification)}`
     );
 
-    if (existingNotification) {
-      console.log(
-        `[notificationStore/addNotification]: Notification already exists: ${existingNotification.id}`
+    const existingNotifications = get(userNotificationsAtom);
+    if (Object.keys(existingNotifications).length === 0) {
+      console.warn(
+        '[notificationStore/addNotificationAtom]: Existing notifications are empty. This might be an error.'
       );
-      return existingNotification;
     }
 
-    // Create a new notification object with default values
-    const newNotificationToAdd: Omit<Notification, 'id'> = {
-      userId: newNotification.userId,
-      title: newNotification.title,
-      message: newNotification.message,
-      type: newNotification.type,
-      status: 'unread',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      expiresAt: new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000), // 30 days
-    };
+    try {
+      // Generate a hash-based notification ID asynchronously
+      const notificationId = await generateNotificationId(
+        newNotification.userId,
+        newNotification.title,
+        newNotification.message,
+        newNotification.type
+      );
 
-    // Check if listingId is provided
-    if (newNotification.listingId) {
-      newNotificationToAdd.listingId = newNotification.listingId;
+      // Check if the notification already exists in the Recoil state
+      const existingNotification = existingNotifications[notificationId];
+      if (existingNotification) {
+        console.log(
+          `[notificationStore/addNotificationAtom]: Notification already exists: ${existingNotification.id}`
+        );
+        return existingNotification;
+      }
+
+      // Create a new notification object with default values
+      const newNotificationToAdd: Omit<Notification, 'id'> = {
+        userId: newNotification.userId,
+        title: newNotification.title,
+        message: newNotification.message,
+        type: newNotification.type,
+        status: 'unread',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        expiresAt: new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000), // 30 days
+      };
+
+      // Check if listingId or matchId is provided
+      if (newNotification.listingId) {
+        newNotificationToAdd.listingId = newNotification.listingId;
+      }
+      if (newNotification.matchId) {
+        newNotificationToAdd.matchId = newNotification.matchId;
+      }
+
+      // Check Firestore to see if a document with this ID already exists
+      const docRef = doc(db, 'Notifications', notificationId);
+      const docSnapshot = await getDoc(docRef);
+
+      if (docSnapshot.exists()) {
+        console.log(
+          `[notificationStore/addNotificationAtom]: Notification with ID ${notificationId} already exists in Firestore.`
+        );
+        const existingFirestoreNotification = docSnapshot.data() as Notification;
+
+        // Update Recoil state with the existing notification
+        set(userNotificationsAtom, (prev) => ({
+          ...prev,
+          [notificationId]: existingFirestoreNotification,
+        }));
+
+        return existingFirestoreNotification;
+      } else {
+        // Add the new notification to Firestore using the generated ID
+        await setDoc(docRef, newNotificationToAdd); // Uses `setDoc` to create/update a doc with a custom ID
+
+        // Update the userNotifications atom with the new notification
+        const notification: Notification = { id: notificationId, ...newNotificationToAdd };
+        set(userNotificationsAtom, (prev) => ({ ...prev, [notificationId]: notification }));
+
+        console.log(
+          `[notificationStore/addNotificationAtom]: New notification added with ID ${notificationId}.`
+        );
+        return notification;
+      }
+    } catch (error) {
+      console.error(`[notificationStore/addNotification]: error: ${error}`);
+      throw error;
     }
-
-    // Check if matchId is provided
-    if (newNotification.matchId) {
-      newNotificationToAdd.matchId = newNotification.matchId;
-    }
-
-    // Add the new notification to Firestore
-    console.log('🔥 [notificationStore/addNotification]');
-    const docRef = await addDoc(collection(db, 'Notifications'), newNotificationToAdd);
-
-    // Update the userNotifications atom with the new notification
-    const notification: Notification = { id: docRef.id, ...newNotificationToAdd };
-    setNotifications((prev) => ({ ...prev, [docRef.id]: notification }));
-    return notification;
-  } catch (error) {
-    console.error(`[notificationStore/addNotification]: error: ${error}`);
-    throw error;
   }
-};
+);
 
 /**
  * @description Fetch all notifications for a user from Firestore
  * @param {string} userId - The ID of the user to fetch notifications for
- * @param {Function} setNotifications - Function to update the userNotificationsAtom
  * @returns {Promise<Record<string, Notification>>} - A promise that resolves to the fetched notifications
  */
-export const fetchAllUserNotifications = async (
-  userId: string,
-  setNotifications: (update: SetStateAction<Record<string, Notification>>) => void
-): Promise<Record<string, Notification>> => {
+export const fetchAllUserNotificationsAtom = atom(null, async (_, set, userId: string) => {
   console.log(
-    `[notificationStore/fetchAllUserNotifications]: Fetching notifications for user: ${userId}`
+    `[notificationStore/fetchAllUserNotificationsAtom]: Fetching notifications for user: ${userId}`
   );
   try {
-    console.log('🔥 [notificationStore/fetchAllUserNotifications]');
+    console.log('🔥 [notificationStore/fetchAllUserNotificationsAtom]');
     const q = query(collection(db, 'Notifications'), where('userId', '==', userId));
     const querySnapshot = await getDocs(q);
 
     const notifications: Record<string, Notification> = {};
-    const currentTime = new Date();
+    // const currentTime = new Date();
 
     querySnapshot.forEach((doc) => {
       const notificationData = doc.data() as Omit<
@@ -127,115 +157,102 @@ export const fetchAllUserNotifications = async (
       };
 
       // Check if the notification has expired and is not removed
-      if (notification.expiresAt > currentTime && notification.status !== 'removed') {
-        notifications[doc.id] = notification;
-      }
+      notifications[doc.id] = notification;
     });
 
     // Update the userNotifications atom with the fetched notifications
-    setNotifications(notifications);
+    set(userNotificationsAtom, notifications);
+    set(notificationsLoadedAtom, true);
+
     return notifications;
   } catch (error) {
-    console.error(`[notificationStore/fetchAllUserNotifications]: error: ${error}`);
+    console.error(`[notificationStore/fetchAllUserNotificationsAtom]: error: ${error}`);
     throw error;
   }
-};
+});
 
 /**
  * @description Fetch a notification from Firestore by ID
  * @param {string} notificationId - The ID of the notification to fetch
- * @param {Record<string, Notification>} existingNotifications - Existing notifications from the atom
- * @param {Function} setNotifications - Function to update the userNotificationsAtom
  * @returns {Promise<Notification | null>} - A promise that resolves to the fetched notification
  */
-export const fetchNotificationById = async (
-  notificationId: string,
-  existingNotifications: Record<string, Notification>,
-  setNotifications: (update: SetStateAction<Record<string, Notification>>) => void
-): Promise<Notification | null> => {
+export const fetchNotificationByIdAtom = atom(null, async (get, set, notificationId: string) => {
   console.log(
-    `[notificationStore/fetchNotificationById]: Fetching notification: ${notificationId}`
+    `[notificationStore/fetchNotificationByIdAtom]: Fetching notification: ${notificationId}`
   );
 
   // Check if the notification already exists in the atom
+  const existingNotifications = get(userNotificationsAtom);
   if (existingNotifications[notificationId]) {
     return existingNotifications[notificationId];
   }
 
   try {
     // Fetch the notification from Firestore
-    console.log('🔥 [notificationStore/fetchNotificationById]');
+    console.log('🔥 [notificationStore/fetchNotificationByIdAtom]');
     const notificationDoc = await getDoc(doc(db, 'Notifications', notificationId));
 
     // If the notification exists, update the userNotifications atom with the new notification
     if (notificationDoc.exists()) {
       const notificationData = notificationDoc.data() as Omit<Notification, 'id'>;
       const notification: Notification = { id: notificationDoc.id, ...notificationData };
-      setNotifications((prev) => ({ ...prev, [notificationId]: notification }));
+      set(userNotificationsAtom, (prev) => ({ ...prev, [notificationId]: notification }));
       return notification;
     }
   } catch (error) {
-    console.error(`[notificationStore/fetchNotificationById]: error: ${error}`);
+    console.error(`[notificationStore/fetchNotificationByIdAtom]: error: ${error}`);
   }
   return null;
-};
+});
 
 /**
  * @description Mark notifications with a specific status in Firestore and update the userNotifications atom
  * @param {string[]} notificationIds - The IDs of the notifications to mark
  * @param {'read' | 'unread' | 'removed'} status - The status to set for the notifications
- * @param {Function} setNotifications - Function to update the userNotificationsAtom
  * @returns {Promise<void>} - A promise that resolves when the notifications are marked
  */
-export const markNotifications = async (
-  notificationIds: string[],
-  status: 'read' | 'unread' | 'removed',
-  setNotifications: (update: SetStateAction<Record<string, Notification>>) => void
-): Promise<void> => {
-  console.log(
-    `[notificationStore/markNotifications]: Marking notifications as ${status}: ${notificationIds}`
-  );
-  try {
-    const batch = writeBatch(db);
+export const markNotificationsAtom = atom(
+  null,
+  async (_, set, notificationIds: string[], status: 'read' | 'unread' | 'removed') => {
+    console.log(
+      `[notificationStore/markNotificationsAtom]: Marking notifications as ${status}: ${notificationIds}`
+    );
+    try {
+      const batch = writeBatch(db);
 
-    notificationIds.forEach((id) => {
-      const notificationRef = doc(db, 'Notifications', id);
-      batch.update(notificationRef, { status, updatedAt: new Date() });
-    });
-
-    console.log('🔥 [notificationStore/markNotifications]');
-    await batch.commit();
-
-    // Update the userNotifications atom
-    setNotifications((prev) => {
-      const updated = { ...prev };
       notificationIds.forEach((id) => {
-        if (updated[id]) {
-          updated[id] = { ...updated[id], status, updatedAt: new Date() };
-        }
+        const notificationRef = doc(db, 'Notifications', id);
+        batch.update(notificationRef, { status, updatedAt: new Date() });
       });
-      return updated;
-    });
-  } catch (error) {
-    console.error(`[notificationStore/markNotifications]: error: ${error}`);
-    throw error;
-  }
-};
 
-// Remove the old markNotificationsAsRead and markNotificationsAsRemoved functions
+      console.log('🔥 [notificationStore/markNotificationsAtom]');
+      await batch.commit();
+
+      // Update the userNotifications atom
+      set(userNotificationsAtom, (prev) => {
+        const updated = { ...prev };
+        notificationIds.forEach((id) => {
+          if (updated[id]) {
+            updated[id] = { ...updated[id], status, updatedAt: new Date() };
+          }
+        });
+        return updated;
+      });
+    } catch (error) {
+      console.error(`[notificationStore/markNotificationsAtom]: error: ${error}`);
+      throw error;
+    }
+  }
+);
 
 /**
  * @description Delete multiple notifications from Firestore and update the userNotifications atom
  * @param {string[]} notificationIds - The IDs of the notifications to delete
- * @param {Function} setNotifications - Function to update the userNotificationsAtom
  * @returns {Promise<void>} - A promise that resolves when the notifications are deleted
  */
-export const deleteNotifications = async (
-  notificationIds: string[],
-  setNotifications: (update: SetStateAction<Record<string, Notification>>) => void
-): Promise<void> => {
+export const deleteNotificationsAtom = atom(null, async (_, set, notificationIds: string[]) => {
   console.log(
-    `[notificationStore/deleteNotifications]: Deleting notifications: ${notificationIds}`
+    `[notificationStore/deleteNotificationsAtom]: Deleting notifications: ${notificationIds}`
   );
   try {
     const batch = writeBatch(db);
@@ -246,11 +263,11 @@ export const deleteNotifications = async (
     });
 
     // Delete the notifications from Firestore
-    console.log('🔥 [notificationStore/deleteNotifications]');
+    console.log('🔥 [notificationStore/deleteNotificationsAtom]');
     await batch.commit();
 
     // Update the userNotifications atom by removing the deleted notifications
-    setNotifications((prev) => {
+    set(userNotificationsAtom, (prev) => {
       const updated = { ...prev };
       notificationIds.forEach((id) => {
         delete updated[id];
@@ -258,7 +275,39 @@ export const deleteNotifications = async (
       return updated;
     });
   } catch (error) {
-    console.error(`[notificationStore/deleteNotifications]: error: ${error}`);
+    console.error(`[notificationStore/deleteNotificationsAtom]: error: ${error}`);
     throw error;
   }
+});
+
+/* ########## HELPER FUNCTIONS ########## */
+
+/**
+ * @description Generate a 20-character ID
+ * @param {string} userId - The ID of the user
+ * @param {string} title - The title of the notification
+ * @param {string} message - The message of the notification
+ * @param {string} type - The type of the notification
+ * @returns {string} - A 20-character ID
+ */
+const generateNotificationId = async (
+  userId: string,
+  title: string,
+  message: string,
+  type: string
+): Promise<string> => {
+  const combinedString = `${userId}|${title}|${message}|${type}`;
+  const encoder = new TextEncoder();
+  const data = encoder.encode(combinedString);
+
+  // Generate the SHA-256 hash using Web Crypto API
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+
+  // Convert the buffer to a base64 string
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // Convert buffer to byte array
+  const hashString = hashArray.map((byte) => byte.toString(16).padStart(2, '0')).join(''); // Convert bytes to hex
+  const base64Hash = btoa(hashString); // Convert hex to base64
+
+  // Truncate the base64-encoded string to 20 characters
+  return base64Hash.substring(0, 20);
 };
